64fc04a36ce3fc52e60a4cd4c76e3906c8e47dac
Uítalo <uitalo.urss@gmail.com>
2022-07-27 22:04:26 -0300

    configurações iniciais do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <h1>Olá</h1>
+    <>
+      <h1>Olá</h1>
+
+      <GlobalStyle />
+    </>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box
+}
+
+body{
+  background: #333;
+  color: #fff;
+}
+
+body, input, textarea, button{
+  font-family: 'Roboto', sans-serif;
+}
+`
