eff63ae1f07d13fc7f751cafd26507129ce33149
Uítalo <uitalo.urss@gmail.com>
2023-05-31 11:51:30 -0300

    especificando os temas padrões do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyled } from "./styles/global"
+
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <GlobalStyled />
+    </ThemeProvider>
   )
 }
