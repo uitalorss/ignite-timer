803c0d21761e8f3f9b2a2c588033a553a3d0d0a3
Uítalo <uitalo.urss@gmail.com>
2023-05-31 12:02:24 -0300

    criação das rotas do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyled } from "./styles/global"
-
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyled } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyled />
     </ThemeProvider>
   )
