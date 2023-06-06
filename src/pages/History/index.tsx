561bc7f9db325aebbfd5109e1582782d28e92283
Uítalo <uitalo.urss@gmail.com>
2023-06-05 21:01:15 -0300

    estilização da pagina de historico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,46 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History(){
   return(
-    <h1>History</h1>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há uma semana</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há uma semana</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>20 minutos</td>
+              <td>Há uma semana</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
